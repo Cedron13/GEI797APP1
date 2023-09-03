38cebfe841f9f8cb9213d1bc497ba1e880fd2f0c
Cloé Léglise <cloe.leglise@free.fr>
2023-09-03 13:06:56 -0400

    Change variable names to fit convention + added menu + translated some comments

--- GEI797Labo/Views/GameView.cs
+++ GEI797Labo/Views/GameView.cs
@@ -15,16 +15,16 @@ namespace GEI797Labo
 {
     internal class GameView
     {
-        private GameForm oGameForm; // Supposons que GameForm est une classe existante
+        private GameForm oGameForm; // Let GameForm be an existing class
         private TileManager tileManager;
         private IController controller;
-        private int displaywidth;
-        private int displayheight;
-        private int tailletuile = 50;
-        private int taillemin;
-        private int leftmargin = 25;
-        private int topmargin = 100;
-        private int middletuile = 12;
+        private int displayWidth;
+        private int displayHeight;
+        private int brickSize = 50;
+        private int minSize;
+        private int leftMargin = 25;
+        private int topMargin = 25;
+        private int brickMiddle = 12;
         private Thread windowThread;
 
 
@@ -74,10 +74,31 @@ namespace GEI797Labo
         {
 
             Graphics g = e.Graphics;
-            // Mettre l'arrière-plan en noir
+            // Black background and menu
             g.Clear(Color.Black);
 
-            //g.DrawImage(tileManager.getImage("Title").bitmap, 20, 48);
+            g.DrawImage(tileManager.getImage("Title").bitmap, leftMargin, topMargin, brickSize * 2, brickSize / 2);
+
+            g.DrawImage(tileManager.getImage("Heart").bitmap, leftMargin + brickSize * 11 /4, topMargin, brickSize / 2, brickSize / 2);
+            g.DrawImage(tileManager.getImage("BeginBar").bitmap, leftMargin + brickSize * 3, topMargin, brickSize / 2, brickSize / 2);
+            g.DrawImage(tileManager.getImage("RedFull").bitmap, leftMargin + brickSize * 7/2, topMargin, brickSize / 2, brickSize / 2);
+            g.DrawImage(tileManager.getImage("RedHalf").bitmap, leftMargin + brickSize * 4, topMargin, brickSize / 2, brickSize / 2);
+            g.DrawImage(tileManager.getImage("EndBar").bitmap, leftMargin + brickSize * 9/2, topMargin, brickSize / 2, brickSize / 2);
+
+            g.DrawImage(tileManager.getImage("BigBubble").bitmap, leftMargin + brickSize * 21/4, topMargin, brickSize / 2, brickSize / 2);
+            g.DrawImage(tileManager.getImage("BeginBar").bitmap, leftMargin + brickSize * 11/2, topMargin, brickSize / 2, brickSize / 2);
+            g.DrawImage(tileManager.getImage("BlueFull").bitmap, leftMargin + brickSize * 6, topMargin, brickSize / 2, brickSize / 2);
+            g.DrawImage(tileManager.getImage("BlueHalf").bitmap, leftMargin + brickSize * 13/2, topMargin, brickSize / 2, brickSize / 2);
+            g.DrawImage(tileManager.getImage("EndBar").bitmap, leftMargin + brickSize * 7, topMargin, brickSize / 2, brickSize / 2);
+
+            g.DrawImage(tileManager.getImage("Gem").bitmap, leftMargin + brickSize * 31/4, topMargin, brickSize / 2, brickSize / 2);
+            g.DrawImage(tileManager.getImage("BeginBar").bitmap, leftMargin + brickSize * 8, topMargin, brickSize / 2, brickSize / 2);
+            g.DrawImage(tileManager.getImage("YellowFull").bitmap, leftMargin + brickSize * 17/2, topMargin, brickSize / 2, brickSize / 2);
+            g.DrawImage(tileManager.getImage("YellowHalf").bitmap, leftMargin + brickSize * 9, topMargin, brickSize / 2, brickSize / 2);
+            g.DrawImage(tileManager.getImage("EndBar").bitmap, leftMargin + brickSize * 19/2, topMargin, brickSize / 2, brickSize / 2);
+
+            //g.DrawImage(tileManager.getImage("Key").bitmap, leftMargin + brickSize * 10, topMargin, brickSize / 2, brickSize / 2);
+
 
             //Calls Lab from another thread, lock may be needed
             int[,] labyrinth = controller.GetLabyrinth();
@@ -89,15 +110,15 @@ namespace GEI797Labo
                 {
                     if (labyrinth[j, i] == 1)
                     {
-                        g.DrawImage(tileManager.getImage("Wall").bitmap, tailletuile * i + leftmargin, tailletuile * j + topmargin, tailletuile, tailletuile);
+                        g.DrawImage(tileManager.getImage("Wall").bitmap, brickSize * i + leftMargin, brickSize * j + 2 * topMargin + brickSize/2, brickSize, brickSize);
                     }
                     else if (labyrinth[j, i] == 2)
                     {
-                        g.DrawImage(tileManager.getImage("Wall").bitmap, tailletuile * i + leftmargin, tailletuile * j + topmargin, tailletuile, tailletuile);
-                        //TODO : modify transparency
+                        g.DrawImage(tileManager.getImage("Wall").bitmap, brickSize * i + leftMargin, brickSize * j + 2 * topMargin + brickSize / 2, brickSize, brickSize);
+
                         using (Brush yellowBrush = new SolidBrush(Color.FromArgb(150, Color.Black)))
                         {
-                            g.FillRectangle(yellowBrush, new Rectangle(tailletuile * i + leftmargin, tailletuile * j + topmargin, tailletuile, tailletuile));
+                            g.FillRectangle(yellowBrush, new Rectangle(brickSize * i + leftMargin, brickSize * j + 2 * topMargin + brickSize / 2, brickSize, brickSize));
                         }
                     }
                     else if (labyrinth[j, i] == 3)
@@ -106,11 +127,11 @@ namespace GEI797Labo
                     }
                     else if (labyrinth[j, i] == 4)
                     {
-                        g.DrawImage(tileManager.getImage("Gem").bitmap, tailletuile * i + leftmargin + middletuile, tailletuile * j + topmargin + middletuile, tailletuile / 2, tailletuile / 2);
+                        g.DrawImage(tileManager.getImage("Gem").bitmap, brickSize * i + leftMargin + brickMiddle, brickSize * j + 2 * topMargin + brickSize / 2 + brickMiddle, brickSize / 2, brickSize / 2);
                     }
                     else if (labyrinth[j, i] == 5)
                     {
-                        g.DrawImage(tileManager.getImage("MiniSlime").bitmap, tailletuile * i + leftmargin + middletuile, tailletuile * j + topmargin + middletuile, tailletuile / 2, tailletuile / 2);
+                        g.DrawImage(tileManager.getImage("MiniSlime").bitmap, brickSize * i + leftMargin + brickMiddle, brickSize * j + 2 * topMargin + brickSize / 2 + brickMiddle, brickSize / 2, brickSize / 2);
                     }
                 }
 
@@ -138,13 +159,13 @@ namespace GEI797Labo
         private void GameForm_SizeChanged(object sender, EventArgs e)
         {
             //Console.WriteLine("coco");
-            displayheight = oGameForm.Size.Height;
-            displaywidth = oGameForm.Size.Width;
-            taillemin = Math.Min(displayheight, displaywidth); // On priorise la taille la plus faible
-            tailletuile = (int)((taillemin / 600.0) * 50); // Adaptation taille des tuiles
-            leftmargin = (int)(tailletuile / 2);
-            topmargin = (int)(tailletuile * 2);
-            middletuile = (int)(tailletuile / 4);
+            displayHeight = oGameForm.Size.Height;
+            displayWidth = oGameForm.Size.Width;
+            minSize = Math.Min(displayHeight, displayWidth); // Smaller size is the priority
+            brickSize = (int)((minSize / 600.0) * 50); // Adapting brick sizes
+            leftMargin = (int)(brickSize / 2);
+            topMargin = (int)(brickSize / 2);
+            brickMiddle = (int)(brickSize / 4);
 
         }
 
