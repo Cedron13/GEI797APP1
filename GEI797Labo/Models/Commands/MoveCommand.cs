ec819a096323e4c9c5ae45aa6dac3836ae64d78b
Cedric Charron <chac0902@usherbrooke.ca>
2023-09-12 11:34:45 -0400

    Added Move Command

---
+++ GEI797Labo/Models/Commands/MoveCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GEI797Labo.Models.Commands
+{
+    internal class MoveCommand: IGameCommand
+    {
+        private Direction dir;
+        public MoveCommand(Direction d) {
+            dir = d;
+        }
+
+        public void Execute() { }
+    }
+}
