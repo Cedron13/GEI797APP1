386cc47d3bb2f16ad7fdf64b7f696a7d63ca9764
Cedric Charron <chac0902@usherbrooke.ca>
2023-09-21 18:32:57 -0400

    Added State pause and resume tests

---
+++ Explorus-E_Tests/TestControllerState.cs
@@ -0,0 +1,35 @@
+﻿using ExplorusE.Controllers.States;
+using ExplorusE.Models;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    [TestClass]
+    public class TestControllerState
+    {
+        [TestMethod]
+        public void TestPause()
+        {
+            ControllerMOC moc = new ControllerMOC();
+            GameModel gm = new GameModel(moc);
+            IState currentState;
+            PlayState play = new PlayState(moc);
+            currentState = play;
+            play.PrepareNextState(ExplorusE.Constants.GameStates.PAUSE);
+            currentState = play.GetNextState();
+            Assert.IsTrue(currentState is PausedState);
+        }
+        [TestMethod]
+        public void TestResume()
+        {
+            ControllerMOC moc = new ControllerMOC();
+            GameModel gm = new GameModel(moc);
+            IState currentState;
+            PausedState pause = new PausedState(moc);
+            currentState = pause;
+            pause.PrepareNextState(ExplorusE.Constants.GameStates.RESUME);
+            currentState = pause.GetNextState();
+            Assert.IsTrue(currentState is ResumeState);
+        }
+    }
+}
