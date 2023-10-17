b2996b10408ae133d5085e40e12af67648e69859
Cedric <ccharr00@gmail.com>
2023-10-17 12:46:27 -0400

    Fixed collision after death

--- Explorus-E/Controllers/Controller.cs
+++ Explorus-E/Controllers/Controller.cs
@@ -305,6 +305,7 @@ namespace ExplorusE.Controllers
                             currentState.GetNextState();
 
                         }
+                        model.SetIsPaused(false);
 
                     }
                 }
