9f6ad657e026f9a150918dd408e8344b322e37ec
blatrixa <alexis.blatrix@etu.univ-savoie.fr>
2023-10-18 16:41:29 -0400

    Add resources sound + class audiothread

---
+++ Explorus-E/Threads/AudioThreadBubble.cs
@@ -0,0 +1,45 @@
+﻿using ExplorusE.Constants;
+using ExplorusE.Models;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplorusE.Threads
+{
+    internal class AudioThreadBubble
+    {
+        private bool isRunning = true;
+        private readonly object lockObj = new object();
+
+        //private RenderList permanentItems = new RenderList();
+
+        /// <summary>
+        /// Stops the RenderThread's thread
+        /// </summary>
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public void Stop()
+        {
+            isRunning = false;
+        }
+
+
+
+        /// <summary>
+        /// Runs the thread
+        /// </summary>
+        public void Run()
+        {
+            Console.WriteLine("START - RenderThread");
+            while (isRunning)
+            {
+
+                //Thread.Sleep(1);
+            }
+            Console.WriteLine("STOP - RenderThread");
+        }
+    }
+}
