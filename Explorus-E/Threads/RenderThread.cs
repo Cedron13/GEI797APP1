f71e56d6dd55284036e5a436a7115f1d9a783280
Audric DAVID <audric.david@usherbrooke.ca>
2023-10-19 17:45:14 -0400

    FSP ???????

--- Explorus-E/Threads/RenderThread.cs
+++ Explorus-E/Threads/RenderThread.cs
@@ -10,6 +10,33 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
+
+/*
+ * 
+ *
+const N = 2
+range T = 0..N
+
+GAMELOOP = (askForNewItem -> GAMELOOP).
+RENDERTHREAD = (waitItemInQueue -> addPermanentItem -> removeItemInQueue -> RENDERTHREAD
+				| waitItemInQueue -> addItem -> removeItemInQueue -> RENDERTHREAD).
+WINDOWTHREAD = (getList -> WINDOWTHREAD
+				| getPermanentList -> WINDOWTHREAD).
+QUEUE = ARRAY[0], ARRAY[i:T] =
+	(when(i<N) askForNewItem -> ARRAY[i+1]
+	|when(i>0) removeItemInQueue -> ARRAY[i-1]).
+PERMANENTLIST = ARRAY[0], ARRAY[i:T] =
+	(when(i<N) add-> ARRAY[i+1]
+	|clear -> ARRAY[0]).
+NONPERMANENTLIST = ARRAY[0], ARRAY[i:T] =
+	(when(i<N) add-> ARRAY[i+1]
+	|clear -> ARRAY[0]).
+
+||THREADS = (GAMELOOP||RENDERTHREAD||WINDOWTHREAD||QUEUE||PERMANENTLIST||NONPERMANENTLIST).
+ * 
+ * 
+ */
+
 namespace ExplorusE.Threads
 {
     internal class RenderThread
