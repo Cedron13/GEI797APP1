9c6e4338f4b1ee5ce3ccf760c2109c2ffcef6e1a
Audric DAVID <audric.david@usherbrooke.ca>
2023-10-19 18:56:54 -0400

    controller diff

--- Explorus-E/Controllers/Controller.cs
+++ Explorus-E/Controllers/Controller.cs
@@ -347,12 +347,12 @@ namespace ExplorusE.Controllers
             {
                 s.NotifyResize(view.GetTopMargin(), view.GetLeftMargin(), view.GetBrickSize());
             }
-            foreach (Wall w in walls) queue.AskForNewItem(w, RenderItemType.Permanent); //Re-adding all walls in the render list
-            queue.AskForNewItem(titleSprite, RenderItemType.Permanent);
-            queue.AskForNewItem(heartSprite, RenderItemType.Permanent);
-            queue.AskForNewItem(bubbleSprite, RenderItemType.Permanent);
-            queue.AskForNewItem(coinSprite, RenderItemType.Permanent);
-            queue.AskForNewItem(miniSlime, RenderItemType.Permanent);
+            foreach (Wall w in walls) oRenderThread.AskForNewItem(w, RenderItemType.Permanent); //Re-adding all walls in the render list
+            oRenderThread.AskForNewItem(titleSprite, RenderItemType.Permanent);
+            oRenderThread.AskForNewItem(heartSprite, RenderItemType.Permanent);
+            oRenderThread.AskForNewItem(bubbleSprite, RenderItemType.Permanent);
+            oRenderThread.AskForNewItem(coinSprite, RenderItemType.Permanent);
+            oRenderThread.AskForNewItem(miniSlime, RenderItemType.Permanent);
         }
 
         public void EngineProcessInputEvent()
@@ -614,6 +614,7 @@ namespace ExplorusE.Controllers
 
             helpMenu = new HelpMenu(view.GetTopMargin(), view.GetLeftMargin(), view.GetBrickSize());
             AddSubscriber(pauseMenu);
+            AddSubscriber(helpMenu);
         }
 
 
